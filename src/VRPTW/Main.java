561c70a50a8493ce59adc33e6caa1997105fe0ba
86155 <1932813134@qq.com>
2022-03-17 15:01:11 +0800

    编码方式（GBK）更改完毕！

--- src/VRPTW/Main.java
+++ src/VRPTW/Main.java
@@ -4,20 +4,19 @@ import static VRPTW.InitAndPrint.*;
 import static VRPTW.TS.*;
 
 public class Main {
-	public static void main (String arg[]) {
-		
+	public static void main(String arg[]) {
+
 		long begintime = System.nanoTime();
-		
+
 		ReadIn();
-	    Construction();
-	    TabuSearch();
-	    Output();
-	    CheckAns();
-		
-	    long endtime = System.nanoTime();
-		double usedTime= (endtime - begintime)/(1e9);
+		Construction();
+		TabuSearch();
+		Output();
+		CheckAns();
+
+		long endtime = System.nanoTime();
+		double usedTime = (endtime - begintime) / (1e9);
 		System.out.println();
-		System.out.println("Total run time £º"+usedTime+"s");
-		System.out.println();
+		System.out.println("Total run time £º" + usedTime + "s");
 	}
 }
